17ed695868ec26ac0e924e60a14885fd13e570b2
Lorenzo Cunial <99892625+d3uk4li0n@users.noreply.github.com>
2024-01-12 18:22:34 +0100

    Update bangrab.py

--- bangrab.py
+++ bangrab.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from sys import argv
 import socket
 
@@ -9,7 +11,7 @@ def get_banner(ip, port=80, timeout=10, query="GET / HTTP/3\r\n\n\n"):
     s.settimeout(timeout)
     s.connect((hname, port))
     s.send(query.encode()) ## do we need to .encode('utf-8') it?
-    print(s.recv(10000))
+    print(s.recv(10000).decode())
     s.close()
 
 def get_domain_name(ip):
@@ -21,4 +23,10 @@ def get_domain_name(ip):
         return ""
 
 if __name__ == "__main__":
-    print(get_domain_name(argv[1]))
+    parser = argparse.ArgumentParser(prog="banner grabber", description="Your favorite banner grabber tool in python",
+                                     epilog="python3 bangrab.py [port] [timeout] [GET]|[HEAD]")
+    parser.add_argument("ip or hostname", help="ip or hostname of the target")
+    parser.add_argument("-p", "--port", required=False, help="Select port number (default 80)")
+    parser.add_argument("-t", "--timeout", required=False, help="Set timeout (default 10 seconds)")
+    parser.add_argument("-q", "--query", required=False)
+    #print(get_domain_name(argv[1]))
